dfdb5f5e5a30f88da4887ae2eabdb08ef7b8235e
mpetkevic <miroslav.petkevic@gmail.com>
2017-11-12 20:12:27 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+    $('#phone').mask(' +7 (999) 99-99-999')
+});
+
+$(document).ready(function() {
+    
+        //E-mail Ajax Send
+        $("#form-top").submit(function() { //Change
+            var th = $(this);
+            $.ajax({
+                type: "POST",
+                url: "mail.php", //Change
+                data: th.serialize()
+            }).done(function() {
+                alert("Thank you!");
+                setTimeout(function() {
+                    // Done Functions
+                    th.trigger("reset");
+                }, 1000);
+            });
+            return false;
+        });
+    
+    });
